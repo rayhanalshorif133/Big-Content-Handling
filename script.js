f13e8be097063201c3b652e7d2f7e4b075385164
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-04-27 10:54:11 +0600

    Content-Handling

---
+++ script.js
@@ -0,0 +1,25 @@
+/*
+* Title: 
+* Description: 
+* Author: Rayhan Al Shorif
+* Email: rayhanalshorif@gmail.com
+* Date: 
+*/
+
+// Dependencies
+const http = require('http');
+const fs = require('fs');
+
+// App object - module scaffolding
+const app = {};
+
+// Configuration
+
+
+// Create server
+app.main = () => {
+    console.log("Hello World");
+};
+
+// start the server
+app.main();
